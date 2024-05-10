b5d3b3929a893c8e25273b811e8d2a64fd369e0c
TimofeyDerbugov <derbugov.d75@gmail.com>
2024-05-11 00:01:36 +0300

    Изменение цветов при разной температуре

--- ViewController.swift
+++ ViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let tempView = UIView()
     let tempLabel = UILabel()
     let iconWeather = UIImageView()
     let updateTapButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .darkGray
         
         tempLabel.frame = CGRect(x: (view.frame.size.width / 2 - 100), y: 50, width: 200, height: 200)
         tempLabel.text = "--°"
@@ -44,7 +45,24 @@ class ViewController: UIViewController {
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
                 DispatchQueue.main.async {
+                    var a = weather.currentWeather.temperature
                     self.tempLabel.text = "\(weather.currentWeather.temperature) °"
+                    if weather.currentWeather.temperature < -10 {
+                        self.tempLabel.textColor = UIColor(red: 0.00, green: 0.33, blue: 2.1, alpha: 1.0)
+                    }
+                    if a >= -10 && a <= 0 {
+                        self.tempLabel.textColor = UIColor(red: 0.00, green: 1.40, blue: 2.1, alpha: 1.0)
+                    }
+                    if a >= 1 && a <= 10 {
+                        self.tempLabel.textColor = UIColor(red: 1.79, green: 2.30, blue: 0.00, alpha: 1.0)
+                    }
+                    if a >= 11 && a <= 25 {
+                        self.tempLabel.textColor = UIColor(red: 2.30, green: 1.04, blue: 0.00, alpha: 1.0)
+                    }
+                    else {
+                        self.tempLabel.textColor = UIColor(red: 2.30, green: 1.04, blue: 0.00, alpha: 1.0)
+                    }
+                    
                 }
             } else {
                 print("Fail!")
@@ -52,6 +70,9 @@ class ViewController: UIViewController {
         }
         task.resume()
     }
+    
 
 }
+
+
 
